3d0943bdc6e8cfe772c0f2ef6f09a80c4fb48222
mzelenevskaya <mzelenevskaya@gmail.com>
2022-06-10 11:26:21 +0300

    feat: add html and css layouts for MoGo-landing

---
+++ assets/js/script.js
@@ -0,0 +1,13 @@
+// console.log('111');
+console.log('hello world');
+
+// header
+let header = document.querySelector(".header");
+
+document.addEventListener("scroll", () => {
+  if (document.documentElement.scrollTop > 1) {
+    header.classList.add("header_has-bg");
+  } else {
+    header.classList.remove("header_has-bg");
+  }
+});
